784fa1a7b4b14675aaa9e36ea1ad517e8b4e87b7
Ju8t1n203 <Juxin203@gmail.com>
2025-03-31 23:25:51 -0600

    first

--- GameofWar/GameofWar/Game.vb
+++ GameofWar/GameofWar/Game.vb
@@ -15,28 +15,6 @@ Public Class Game
     End Sub
     Private Sub DealButton_Click(sender As Object, e As EventArgs) Handles DealButton.Click
 
-        ' The code that was associated with using the now obsolete "Shuffler" class
-
-        'Dim dealtCards As Integer(,) = Shuffler.Shuffle() 'shuffle and deal for the player and computer
-
-        ''clears the listboxes before adding new items
-        'PlayerListBox.Items.Clear()
-        'ComputerListBox.Items.Clear()
-
-        'Dim playerIndex As Integer = 0          'indexes the oth column of the shuffled cards
-        'Dim computerIndex As Integer = 1      'indexes the 1st column of the shuffled cards
-
-        ''loops through the cards for the selected index and add them to the corrosponding listbox
-        'For card As Integer = 0 To dealtCards.GetLength(1) - 1
-        '    PlayerListBox.Items.Add(dealtCards(playerIndex, card))
-        'Next
-
-        'For card As Integer = 0 To dealtCards.GetLength(1) - 1
-        '    ComputerListBox.Items.Add(dealtCards(computerIndex, card))
-        'Next
-
-        'Dim deck As New CardGenerator("Deck")
-
         PlayerListBox.Items.Clear()
         ComputerListBox.Items.Clear()
 
